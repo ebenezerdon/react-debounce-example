6083b58f0d1911b9343359eec9c6ff5f9a34bf0f
ebenezerdon <ebenezerdonu@gmail.com>
2021-09-14 10:43:25 +0100

    implement debounce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import cities from 'cities-list'
+import { useState } from 'react'
+import { debounce } from 'lodash'
+import { DebounceInput } from 'react-debounce-input'
 
-function App() {
+const citiesArray = Object.keys(cities)
+
+const App = () => {
+  const [filteredCities, setFilteredCities] = useState([])
+
+  // const doCityFilter = debounce(query => {
+  //   console.log('====>', 'hey')
+  //   if (query) {
+  //     setFilteredCities(citiesArray.filter(
+  //       city => city.toLowerCase().includes(query.toLowerCase())
+  //     ))
+  //   } else {
+  //     setFilteredCities([])
+  //   }
+  // }, 500)
+
+
+  // let filterTimeout
+  // const doCityFilter = query => {
+  //   if (query) {
+  //     clearTimeout(filterTimeout)
+  //
+  //     filterTimeout = setTimeout(() => {
+  //       setFilteredCities(citiesArray.filter(
+  //         city => city.toLowerCase().includes(query.toLowerCase())
+  //       ))
+  //     }, 500)
+  //
+  //   } else {
+  //     setFilteredCities([])
+  //   }
+  // }
+
+  const doCityFilter = query => {
+    console.log('====>', query)
+    if (query) {
+      setFilteredCities(citiesArray.filter(
+        city => city.toLowerCase().includes(query.toLowerCase())
+      ))
+    } else {
+      setFilteredCities([])
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Find your favourite cities</h1>
+
+      <form className="mt-3 mb-5">
+        <DebounceInput
+          className="px-2"
+          placeholder="search here..."
+          minLength={1}
+          debounceTimeout={500}
+          onChange={event => (doCityFilter(event.target.value))}
+        />
+
+        {/*<input*/}
+        {/*  type="text"*/}
+        {/*  className="px-2"*/}
+        {/*  placeholder="search here..."*/}
+        {/*  onChange={event => (doCityFilter(event.target.value))}*/}
+        {/*/>*/}
+      </form>
+
+      <div>
+        {filteredCities?.map((city, index) => (
+          <p key={index}>{city}</p>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
