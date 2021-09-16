c0c454f632800f9af46caca19974dc5c466f42f7
ebenezerdon <ebenezerdonu@gmail.com>
2021-09-16 12:17:49 +0100

    implement debounce without external libraries

--- src/App.js
+++ src/App.js
@@ -1,71 +1,37 @@
 import cities from 'cities-list'
 import { useState } from 'react'
-import { debounce } from 'lodash'
-import { DebounceInput } from 'react-debounce-input'
 
 const citiesArray = Object.keys(cities)
 
 const App = () => {
   const [filteredCities, setFilteredCities] = useState([])
 
-  // const doCityFilter = debounce(query => {
-  //   console.log('====>', 'hey')
-  //   if (query) {
-  //     setFilteredCities(citiesArray.filter(
-  //       city => city.toLowerCase().includes(query.toLowerCase())
-  //     ))
-  //   } else {
-  //     setFilteredCities([])
-  //   }
-  // }, 500)
-
-
-  // let filterTimeout
-  // const doCityFilter = query => {
-  //   if (query) {
-  //     clearTimeout(filterTimeout)
-  //
-  //     filterTimeout = setTimeout(() => {
-  //       setFilteredCities(citiesArray.filter(
-  //         city => city.toLowerCase().includes(query.toLowerCase())
-  //       ))
-  //     }, 500)
-  //
-  //   } else {
-  //     setFilteredCities([])
-  //   }
-  // }
-
+  let filterTimeout
   const doCityFilter = query => {
-    console.log('====>', query)
-    if (query) {
+    clearTimeout(filterTimeout)
+    if (!query) return setFilteredCities([])
+
+    filterTimeout = setTimeout(() => {
+      console.log('====>', query)
       setFilteredCities(citiesArray.filter(
         city => city.toLowerCase().includes(query.toLowerCase())
       ))
-    } else {
-      setFilteredCities([])
-    }
+    }, 500)
+
+    return filterTimeout
   }
 
   return (
     <div className="container">
-      <h1>Find your favourite cities</h1>
+      <h1 className="mt-5">Find your favourite cities</h1>
 
-      <form className="mt-3 mb-5">
-        <DebounceInput
+      <form className="mt-3 mb-3">
+        <input
+          type="text"
           className="px-2"
           placeholder="search here..."
-          minLength={1}
-          debounceTimeout={500}
           onChange={event => (doCityFilter(event.target.value))}
         />
-
-        {/*<input*/}
-        {/*  type="text"*/}
-        {/*  className="px-2"*/}
-        {/*  placeholder="search here..."*/}
-        {/*  onChange={event => (doCityFilter(event.target.value))}*/}
-        {/*/>*/}
       </form>
 
       <div>
