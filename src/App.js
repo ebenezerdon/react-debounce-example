4249f205dc3a88119c10ce1f235d3f9404ca8b74
ebenezerdon <ebenezerdonu@gmail.com>
2021-09-16 10:18:05 +0100

    implement without debounce

--- src/App.js
+++ src/App.js
@@ -1,50 +1,17 @@
 import cities from 'cities-list'
 import { useState } from 'react'
-import { debounce } from 'lodash'
-import { DebounceInput } from 'react-debounce-input'
 
 const citiesArray = Object.keys(cities)
 
 const App = () => {
   const [filteredCities, setFilteredCities] = useState([])
 
-  // const doCityFilter = debounce(query => {
-  //   console.log('====>', 'hey')
-  //   if (query) {
-  //     setFilteredCities(citiesArray.filter(
-  //       city => city.toLowerCase().includes(query.toLowerCase())
-  //     ))
-  //   } else {
-  //     setFilteredCities([])
-  //   }
-  // }, 500)
-
-
-  // let filterTimeout
-  // const doCityFilter = query => {
-  //   if (query) {
-  //     clearTimeout(filterTimeout)
-  //
-  //     filterTimeout = setTimeout(() => {
-  //       setFilteredCities(citiesArray.filter(
-  //         city => city.toLowerCase().includes(query.toLowerCase())
-  //       ))
-  //     }, 500)
-  //
-  //   } else {
-  //     setFilteredCities([])
-  //   }
-  // }
-
   const doCityFilter = query => {
-    console.log('====>', query)
-    if (query) {
-      setFilteredCities(citiesArray.filter(
-        city => city.toLowerCase().includes(query.toLowerCase())
-      ))
-    } else {
-      setFilteredCities([])
-    }
+    if (!query) return setFilteredCities([])
+
+    setFilteredCities(citiesArray.filter(
+      city => city.toLowerCase().includes(query.toLowerCase())
+    ))
   }
 
   return (
@@ -52,20 +19,12 @@ const App = () => {
       <h1>Find your favourite cities</h1>
 
       <form className="mt-3 mb-5">
-        <DebounceInput
+        <input
+          type="text"
           className="px-2"
           placeholder="search here..."
-          minLength={1}
-          debounceTimeout={500}
           onChange={event => (doCityFilter(event.target.value))}
         />
-
-        {/*<input*/}
-        {/*  type="text"*/}
-        {/*  className="px-2"*/}
-        {/*  placeholder="search here..."*/}
-        {/*  onChange={event => (doCityFilter(event.target.value))}*/}
-        {/*/>*/}
       </form>
 
       <div>
